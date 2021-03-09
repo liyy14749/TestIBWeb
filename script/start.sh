36888be7f13eeb1e1e4b3834dcab834249210ab2
Tom <tom@gmail.com>
2021-03-09 22:31:42 +0700

    调整启动脚本

--- script/start.sh
+++ script/start.sh
@@ -8,7 +8,7 @@ if [ $num -gt 0 ]; then
   echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: is running." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.log"
 else
   echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: start to running." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.log"
-  nohup java -jar ${PROJECT_NAME_JAR} --spring.profiles.active=${ACTIVE} --spring.profiles.clientId=${CLIENT_ID} >> ${PROJECT_CONSOLE_LOG_PATH}/console.log 2>&1 &
+  nohup java -jar ${PROJECT_NAME_JAR} --spring.profiles.active=${ACTIVE} --my.ib.server.clientId=${CLIENT_ID} >> ${PROJECT_CONSOLE_LOG_PATH}/console.log 2>&1 &
   sleep 1
   tail -fn 50 ${PROJECT_CONSOLE_LOG_PATH}/console.log
 fi
