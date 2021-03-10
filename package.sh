b344589f549353cd5658df3578e98793f3286a48
Tom <tom@gmail.com>
2021-03-10 12:02:34 +0700

    调整打包及启动脚本

--- package.sh
+++ package.sh
@@ -7,9 +7,9 @@ mvn clean install -Dmaven.test.skip=true
 echo "------------------------------ mvn install [end] ------------------------------"
 
 echo "------------------------------ package distribute [start] ------------------------------"
-cp -f target/IBWebServer.jar ${PROJECT_NAME}
-cp -f script/*.sh ${PROJECT_NAME} && chmod +x ${PROJECT_NAME}/*.sh
+cp -f target/IBWebServer.jar ${PROJECT_PATH}
+cp -f script/*.sh ${PROJECT_PATH} && chmod +x ${PROJECT_PATH}/*.sh
 
-ls -al ${PROJECT_NAME}
+ls -al ${PROJECT_PATH}
 echo "------------------------------ package distribute [end] ------------------------------"
 echo "distribute success."
