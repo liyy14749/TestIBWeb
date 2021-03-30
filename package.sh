de214f22c6b62c82d090b8b0cd8c7b12f72d1ac8
Tom <tom@gmail.com>
2021-03-31 01:27:02 +0700

    调整启动脚本

--- package.sh
+++ package.sh
@@ -7,8 +7,8 @@ mvn clean install -Dmaven.test.skip=true
 echo "------------------------------ mvn install [end] ------------------------------"
 
 echo "------------------------------ package distribute [start] ------------------------------"
-cp -f target/IBWebServer.jar ${PROJECT_PATH}
-cp -f script/*.sh ${PROJECT_PATH} && chmod +x ${PROJECT_PATH}/*.sh
+\cp -f target/IBWebServer.jar ${PROJECT_PATH}
+\cp -f script/*.sh ${PROJECT_PATH} && chmod +x ${PROJECT_PATH}/*.sh
 
 ls -al ${PROJECT_PATH}
 echo "------------------------------ package distribute [end] ------------------------------"
--- script/start.sh
+++ script/start.sh
@@ -12,7 +12,7 @@ else
   time_hour=$(date "+%Y%m%d%H");
   log_path="${PROJECT_CONSOLE_LOG_PATH}/console.${time_hour}.log"
   nohup java -jar ${PROJECT_NAME_JAR} --spring.profiles.active=${ACTIVE} --my.ib.server.clientId=${CLIENT_ID} >> ${log_path} 2>&1 &
-  sleep 1
-  tail -fn 50 ${log_path}
+  sleep 2
+  tail -n 50 ${log_path}
 fi
 
