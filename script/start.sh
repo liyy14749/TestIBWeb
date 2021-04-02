3aaa456a5b2ecbd43c2aa73d462c20f88c95e1b0
Tom <tom@gmail.com>
2021-04-02 16:06:06 +0700

    调整启动脚本

--- script/start.sh
+++ script/start.sh
@@ -10,12 +10,12 @@ do
   # 启动服务
   num=$(ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${ACTIVE}" | grep "${CLIENT_ID}" | grep "${client_host}" | grep -v "grep" | wc -l)
   if [ $num -gt 0 ]; then
-    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: is running." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.${client_host}.log"
+    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: is running." >> "${PROJECT_CONSOLE_LOG_PATH}/${client_host}.${PROJECT_NAME}.log"
   else
-    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: start to running." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.${client_host}.log"
+    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: start to running." >> "${PROJECT_CONSOLE_LOG_PATH}/${client_host}.${PROJECT_NAME}.log"
 
-    time_hour=$(date "+%Y%m%d%H");
-    log_path="${PROJECT_CONSOLE_LOG_PATH}/console.${time_hour}.${client_host}.log"
+    time_hour=$(date "+%Y%m%d%H%M");
+    log_path="${PROJECT_CONSOLE_LOG_PATH}/${client_host}.console.${time_hour}.log"
     nohup java -jar ${PROJECT_NAME_JAR} --spring.profiles.active=${ACTIVE} --my.ib.server.clientId=${CLIENT_ID} --my.ib.server.host=${client_host} --server.port=${server_port} >> ${log_path} 2>&1 &
     sleep 3
     tail -n 50 ${log_path}
--- script/stop.sh
+++ script/stop.sh
@@ -10,8 +10,8 @@ do
   num=$(ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${ACTIVE}" | grep "${CLIENT_ID}" | grep "${client_host}" | grep -v "grep" | wc -l)
   if [ $num -gt 0 ]; then
     ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${ACTIVE}" | grep "${CLIENT_ID}" | grep "${client_host}" | grep -v "grep" | awk '{print $2}' | xargs kill -9
-    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: to stop." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.${client_host}.log"
+    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: to stop." >> "${PROJECT_CONSOLE_LOG_PATH}/${client_host}.${PROJECT_NAME}.log"
   else
-    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: not running." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.${client_host}.log"
+    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: not running." >> "${PROJECT_CONSOLE_LOG_PATH}/${client_host}.${PROJECT_NAME}.log"
   fi
 done
