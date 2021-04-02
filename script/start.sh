50c29d0b319d2eb40a096b746b29ec590de35fb7
Tom <tom@gmail.com>
2021-04-02 15:43:33 +0700

    调整启动脚本

--- script/start.sh
+++ script/start.sh
@@ -15,7 +15,7 @@ do
     echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: start to running." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.${client_host}.log"
 
     time_hour=$(date "+%Y%m%d%H");
-    log_path="${PROJECT_CONSOLE_LOG_PATH}/console.${time_hour}.log"
+    log_path="${PROJECT_CONSOLE_LOG_PATH}/console.${time_hour}.${client_host}.log"
     nohup java -jar ${PROJECT_NAME_JAR} --spring.profiles.active=${ACTIVE} --my.ib.server.clientId=${CLIENT_ID} --my.ib.server.host=${client_host} --server.port=${server_port} >> ${log_path} 2>&1 &
     sleep 3
     tail -n 50 ${log_path}
