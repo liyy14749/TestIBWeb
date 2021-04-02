11927e00e965bba995554b2104c7c951c1733ec9
Tom <tom@gmail.com>
2021-04-02 15:13:17 +0700

    调整启动脚本

--- script/config.sh
+++ script/config.sh
@@ -25,32 +25,20 @@ fi
 
 # 启动环境，默认dev
 ACTIVE="dev"
-CLIENT_ID="2000"
-CLIENT_HOST="127.0.0.1"
-CLIENT_PORT="7496"
+CLIENT_ID="9999"
 if [ "$1" == "-prod" ]; then
   ACTIVE="prod"
-  CLIENT_HOST="10.0.2.25"
-  CLIENT_PORT="40001"
+  CLIENT_PORT="90001"
+elif [ "$1" == "-pre" ]; then
+  ACTIVE="pre"
+  CLIENT_PORT="20001"
 elif [ "$1" == "-test" ]; then
   ACTIVE="test"
 fi
-echo "ACTIVE: ${ACTIVE}"
 
-# 客户端ID
-if [ x"$2" != x ]; then
-  CLIENT_ID=$2
-fi
+echo "ACTIVE: ${ACTIVE}"
 echo "CLIENT_ID: ${CLIENT_ID}"
 
-# 盈透客户端 HOST
-if [ x"$3" != x ]; then
-  CLIENT_HOST=$3
-fi
-echo "CLIENT_HOST: ${CLIENT_HOST}"
+CLIENT_HOST="10.0.2.27"
+SERVER_PORT=8084
 
-# 盈透客户端 PORT
-if [ x"$4" != x ]; then
-  CLIENT_PORT=$4
-fi
-echo "CLIENT_PORT: ${CLIENT_PORT}"
--- script/stop.sh
+++ script/stop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-source ./config.sh $1 $2 $3 $4
+source ./config.sh $1
 
 # 启动服务
 num=$(ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${ACTIVE}" | grep "${CLIENT_ID}" | grep "${CLIENT_HOST}" | grep -v "grep" | wc -l)
 if [ $num -gt 0 ]; then
   ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${ACTIVE}" | grep "${CLIENT_ID}" | grep "${CLIENT_HOST}" | grep -v "grep" | awk '{print $2}' | xargs kill -9
-  echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: to stop." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.log"
+  echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: to stop." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.${CLIENT_HOST}.log"
 else
-  echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: not running." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.log"
+  echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: not running." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.${CLIENT_HOST}.log"
 fi
