522927122b3dd32de86c5c6b517dca43d9e287dc
Tom <tom@gmail.com>
2021-04-19 12:41:17 +0700

    启动脚本调整

--- script/start.sh
+++ script/start.sh
@@ -8,15 +8,21 @@ for item in ${CONFIG[@]}; do
   server_port=$(echo ${item} | awk -F ':' '{print $3}')
   client_id=$(echo ${item} | awk -F ':' '{print $4}')
 
+  time_day=$(date "+%Y%m%d")
+  log_path="${PROJECT_CONSOLE_LOG_PATH}/${client_host}/${client_id}/${time_day}"
+  if [ ! -d "${log_path}" ]; then
+    mkdir -p ${log_path}
+  fi
+
   # 启动服务
   num=$(ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${active}" | grep "${client_id}" | grep "${client_host}" | grep -v "grep" | wc -l)
   if [ $num -gt 0 ]; then
-    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: is running." >>"${PROJECT_CONSOLE_LOG_PATH}/${client_host}.${client_id}.${PROJECT_NAME}.log"
+    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: is running." >>"${log_path}/${PROJECT_NAME}.log"
   else
-    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: start to running." >>"${PROJECT_CONSOLE_LOG_PATH}/${client_host}.${client_id}.${PROJECT_NAME}.log"
+    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: start to running." >>"${log_path}/${PROJECT_NAME}.log"
 
-    time_hour=$(date "+%Y%m%d%H%M")
-    log_path="${PROJECT_CONSOLE_LOG_PATH}/${client_host}.${client_id}.${time_hour}.log"
+    time_hour=$(date "+%H%M")
+    log_path="${log_path}/${time_hour}.log"
     nohup java -jar ${PROJECT_NAME_JAR} \
       --spring.profiles.active=${active} \
       --my.ib.server.clientId=${client_id} \
--- script/stop.sh
+++ script/stop.sh
@@ -7,12 +7,18 @@ for item in ${CONFIG[@]}; do
   client_host=$(echo ${item} | awk -F ':' '{print $2}')
   client_id=$(echo ${item} | awk -F ':' '{print $4}')
 
+  time_day=$(date "+%Y%m%d")
+  log_path="${PROJECT_CONSOLE_LOG_PATH}/${client_host}/${client_id}/${time_day}"
+  if [ ! -d "${log_path}" ]; then
+    mkdir -p ${log_path}
+  fi
+
   # 关闭服务
   num=$(ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${active}" | grep "${client_id}" | grep "${client_host}" | grep -v "grep" | wc -l)
   if [ $num -gt 0 ]; then
     ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${active}" | grep "${client_id}" | grep "${client_host}" | grep -v "grep" | awk '{print $2}' | xargs kill -9
-    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: to stop." >>"${PROJECT_CONSOLE_LOG_PATH}/${client_host}.${client_id}.${PROJECT_NAME}.log"
+    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: to stop." >>"${log_path}/${PROJECT_NAME}.log"
   else
-    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: not running." >>"${PROJECT_CONSOLE_LOG_PATH}/${client_host}.${client_id}.${PROJECT_NAME}.log"
+    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: not running." >>"${log_path}/${PROJECT_NAME}.log"
   fi
 done
